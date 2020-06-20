8df39022d7b050ab8e6afb6b77823f5a6c55b900
alfanzulfian <67086133+alfanzulfian@users.noreply.github.com>
2020-06-20 19:13:57 +0800

    test
    First Commit

---
+++ src/registrasi_dan_rekam_medis/Registrasi_dan_Rekam_Medis.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package registrasi_dan_rekam_medis;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Registrasi_dan_Rekam_Medis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
